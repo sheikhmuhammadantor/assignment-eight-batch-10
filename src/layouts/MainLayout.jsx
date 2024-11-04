684437006eac9f82e344cc5e5602f1b58fd0c6a0
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-11-04 09:49:17 +0600

    Footer Section Done.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "../components/NavBar"
+import Footer from "../components/Footer"
 
 function MainLayout() {
   return (
     <section className="max-w-[1440px] px-12 mx-auto mt-5">
         <NavBar></NavBar>
+        <section className="min-h-60">
         <Outlet></Outlet>
-        <h1 className='text-4xl text-center my-12 p-5 bg-green-500'>MainLayout Footer</h1>
+        </section>
+        <Footer></Footer>
     </section>
   )
 }
